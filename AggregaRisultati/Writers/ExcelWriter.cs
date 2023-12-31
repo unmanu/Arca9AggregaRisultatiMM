1762f68188e0de7db0d72f624e4abecdce86e60b
un Manu <unmanu@gmx.com>
2023-12-31 15:47:40 +0100

    excel writer

---
+++ AggregaRisultati/Writers/ExcelWriter.cs
@@ -0,0 +1,100 @@
+﻿using AggregaRisultati.Models;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Bibliography;
+using DocumentFormat.OpenXml.Spreadsheet;
+
+namespace AggregaRisultati.Writers;
+
+public class ExcelWriter : IWriter
+{
+	public string Write(DirectoryInfo directory, SortedDictionary<string, DifferenceDto> differences, SortedDictionary<string, PolizzaInputDto> polizzeInput, SortedDictionary<string, TimesDto> times)
+	{
+		string outputFile = Path.Combine(directory.FullName, "Differenze.xlsx");
+		using var workbook = new XLWorkbook();
+		var worksheet = workbook.AddWorksheet("differenze");
+		int currentRow = 1;
+		WriteHeader(worksheet, currentRow++);
+		if (polizzeInput.Any())
+		{
+			AggregateAll(worksheet, differences, polizzeInput, times, currentRow);
+		}
+		else
+		{
+			AggregateOnlyDifferences(worksheet, differences, currentRow);
+		}
+		worksheet.Columns().AdjustToContents();
+		worksheet.SheetView.FreezeRows(1);
+		workbook.SaveAs(outputFile);
+		return outputFile;
+	}
+
+	private static void AggregateAll(IXLWorksheet worksheet, SortedDictionary<string, DifferenceDto> differences, SortedDictionary<string, PolizzaInputDto> polizzeInput, SortedDictionary<string, TimesDto> times, int currentRow)
+	{
+		SortedSet<string> keys = GetKeys(differences, polizzeInput);
+		foreach (string key in keys)
+		{
+			differences.TryGetValue(key, out DifferenceDto? difference);
+			polizzeInput.TryGetValue(key, out PolizzaInputDto? polizza);
+			times.TryGetValue(key + "_True", out TimesDto? timesTotale);
+			times.TryGetValue(key + "_False", out TimesDto? timesParziale);
+
+			Aggregator headerAggregator = new(difference, polizza, timesTotale, timesParziale);
+			WriteAggregation(worksheet, currentRow, headerAggregator, false);
+			AdjustRowHeight(worksheet, currentRow);
+			currentRow++;
+		}
+	}
+
+	private static void AdjustRowHeight(IXLWorksheet worksheet, int currentRow)
+	{
+		worksheet.Row(currentRow).AdjustToContents();
+		if (worksheet.Row(currentRow).Height > 100)
+		{
+			worksheet.Row(currentRow).Height = 100;
+		}
+	}
+
+	private static SortedSet<string> GetKeys(SortedDictionary<string, DifferenceDto> differences, SortedDictionary<string, PolizzaInputDto> polizzeInput)
+	{
+		SortedSet<string> keys = new();
+		foreach (var item in differences)
+		{
+			keys.Add(item.Key);
+		}
+		foreach (var item in polizzeInput)
+		{
+			keys.Add(item.Key);
+		}
+		return keys;
+	}
+
+	private void AggregateOnlyDifferences(IXLWorksheet worksheet, SortedDictionary<string, DifferenceDto> differences, int currentRow)
+	{
+		foreach (var difference in differences)
+		{
+			Aggregator headerAggregator = new(difference.Value);
+			WriteAggregation(worksheet, currentRow, headerAggregator, false);
+			AdjustRowHeight(worksheet, currentRow);
+			currentRow++;
+		}
+	}
+
+	private void WriteHeader(IXLWorksheet worksheet, int currentRow)
+	{
+		Aggregator headerAggregator = new();
+		WriteAggregation(worksheet, currentRow, headerAggregator, true);
+		worksheet.Row(currentRow).Style.Font.SetBold(true);
+		worksheet.Row(currentRow).Style.Alignment.SetVertical(XLAlignmentVerticalValues.Center);
+		worksheet.Row(currentRow).Style.Alignment.SetHorizontal(XLAlignmentHorizontalValues.Center);
+		worksheet.Row(currentRow).AdjustToContents();
+		worksheet.Range(currentRow, 1, currentRow, Aggregator.NUMBER_OF_FIELDS).SetAutoFilter(true);
+	}
+	private static void WriteAggregation(IXLWorksheet worksheet, int currentRow, Aggregator headerAggregator, bool header)
+	{
+		for (int i = 0; i < Aggregator.NUMBER_OF_FIELDS; i++)
+		{
+			string value = headerAggregator.Aggregate(i, header);
+			worksheet.Cell(currentRow, i + 1).Value = value;
+		}
+	}
+}
---
+++ AggregaRisultatiTest/Writers/ExcelWriterTest.cs
@@ -0,0 +1,130 @@
+﻿using AggregaRisultati.Models;
+using AggregaRisultati.Writers;
+using ClosedXML.Excel;
+
+namespace AggregaRisultatiTest.Parsers;
+
+public class ExcelWriterTest
+{
+
+	private readonly ExcelWriter _writer;
+	private readonly DirectoryInfo _directoryPath;
+
+	public ExcelWriterTest()
+	{
+		_writer = new ExcelWriter();
+		_directoryPath = new(Path.Combine("Resources", "Output"));
+		if (_directoryPath.Exists)
+		{
+			_directoryPath.Delete(true);
+		}
+		_directoryPath.Create();
+	}
+
+	[Fact]
+	public void Write_NoDifference_ReturnsPathToExcelWithOnlyHeader()
+	{
+		string outputFilePath = _writer.Write(_directoryPath, [], [], []);
+
+		outputFilePath.Should().NotBeNull().And.NotBeEmpty();
+		using var workbook = new XLWorkbook(outputFilePath);
+		workbook.Worksheets.Count().Should().Be(1);
+		var worksheet = workbook.Worksheet(1);
+		worksheet.Should().NotBeNull();
+		worksheet.Name.Should().Be("differenze");
+		var lastCellUsedAddress = worksheet.LastCellUsed().Address;
+		lastCellUsedAddress.RowNumber.Should().Be(1);
+	}
+
+	[Fact]
+	public void Write_MultipleDifferences_ReturnsPathToExcelWithMultipleRows()
+	{
+		SortedDictionary<string, DifferenceDto> differences = new();
+		differences.Add("1", new()
+		{
+			NumeroPolizza = "123456789"
+		});
+		differences.Add("2", new()
+		{
+			NumeroPolizza = "111222333"
+		});
+		differences.Add("3", new()
+		{
+			NumeroPolizza = "444555666"
+		});
+
+		string outputFilePath = _writer.Write(_directoryPath, differences, [], []);
+
+		outputFilePath.Should().NotBeNull().And.NotBeEmpty();
+		using var workbook = new XLWorkbook(outputFilePath);
+		workbook.Worksheets.Count().Should().Be(1);
+		var worksheet = workbook.Worksheet(1);
+		worksheet.Should().NotBeNull();
+		worksheet.Name.Should().Be("differenze");
+		var lastCellUsedAddress = worksheet.LastCellUsed().Address;
+		lastCellUsedAddress.RowNumber.Should().Be(4);
+	}
+
+	[Fact]
+	public void Write_SpecificDifference_ReturnsPathToExcelWithAllTheDataOfTheDifference()
+	{
+		SortedDictionary<string, DifferenceDto> differences = new();
+		differences.Add("1", new()
+		{
+				AgenziaGestione = "001",
+				CodiceProdotto = "961",
+				CodiceSocieta = "341",
+				Categoria = "11",
+				NumeroCollettiva = "0",
+				NumeroPolizza = "123456789",
+				ErroreCics = @"errore cics
+errore seconda riga",
+				ErroreAlbedino = "errore albedino",
+				ImportoNettoCics = "5.500",
+				ImportoLordoCics = "0.000",
+				ImposteLordoCics = "",
+				ImportoNettoAlbedino = "15923.301",
+				ImportoLordoAlbedino = "1.000",
+				ImposteLordoAlbedino = "0.000"
+			}
+		);
+
+		string outputFilePath = _writer.Write(_directoryPath, differences, [], []);
+
+		outputFilePath.Should().NotBeNull().And.NotBeEmpty();
+		using var workbook = new XLWorkbook(outputFilePath);
+		workbook.Worksheets.Count().Should().Be(1);
+		var worksheet = workbook.Worksheet(1);
+		worksheet.Should().NotBeNull();
+		worksheet.Name.Should().Be("differenze");
+		var lastCellUsedAddress = worksheet.LastCellUsed().Address;
+		lastCellUsedAddress.RowNumber.Should().Be(2);
+		int currentRow = 2;
+		DifferenceDto difference = differences.First().Value;
+		worksheet.Cell(currentRow, 3).GetText().Should().Be(difference.AgenziaGestione);
+		worksheet.Cell(currentRow, 4).GetText().Should().Be(difference.CodiceProdotto);
+		worksheet.Cell(currentRow, 5).GetText().Should().Be(difference.CodiceSocieta);
+		worksheet.Cell(currentRow, 6).GetText().Should().Be(difference.Categoria);
+		worksheet.Cell(currentRow, 7).GetText().Should().Be(difference.NumeroCollettiva);
+		worksheet.Cell(currentRow, 8).GetText().Should().Be(difference.NumeroPolizza);
+		var multilineErrorWorksheet = NormalizeNewLine(worksheet.Cell(currentRow, 20).Value.GetText());
+		var multilineErrorInput = NormalizeNewLine(difference.ErroreCics);
+		multilineErrorWorksheet.Should().Be(multilineErrorInput);
+		worksheet.Cell(currentRow, 21).GetText().Should().Be(difference.ErroreAlbedino);
+		worksheet.Cell(currentRow, 14).GetText().Should().Be(difference.ImportoNettoCics);
+		worksheet.Cell(currentRow, 15).GetText().Should().Be(difference.ImportoLordoCics);
+		worksheet.Cell(currentRow, 16).IsEmpty().Should().BeTrue();
+		worksheet.Cell(currentRow, 17).GetText().Should().Be(difference.ImportoNettoAlbedino);
+		worksheet.Cell(currentRow, 18).GetText().Should().Be(difference.ImportoLordoAlbedino);
+		worksheet.Cell(currentRow, 19).GetText().Should().Be(difference.ImposteLordoAlbedino);
+	}
+
+	private string NormalizeNewLine(string? text)
+	{
+		if (text == null)
+		{
+			return "";
+		}
+		return text.Replace("\r\n", "\r").Replace("\n", "\r").Replace("\r", "\r\n");
+	}
+}
