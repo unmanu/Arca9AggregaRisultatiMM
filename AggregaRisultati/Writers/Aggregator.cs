f9e2ef4472b7042261f66d722e10decdd02d3056
un Manu <unmanu@gmx.com>
2023-12-31 13:24:45 +0100

    aggregate all

--- AggregaRisultati/Writers/Aggregator.cs
+++ AggregaRisultati/Writers/Aggregator.cs
@@ -4,7 +4,7 @@ namespace AggregaRisultati.Writers;
 
 public class Aggregator(DifferenceDto? difference = null, PolizzaInputDto? polizza = null, TimesDto? timesTotale = null, TimesDto? timesParziale = null)
 {
-	public const int NUMBER_OF_FIELDS = 16;
+	public const int NUMBER_OF_FIELDS = 30;
 	private DifferenceDto? _difference = difference;
 
 	public string Aggregate(int position, bool header)
@@ -15,22 +15,36 @@ public class Aggregator(DifferenceDto? difference = null, PolizzaInputDto? poliz
 		}
 		return position switch
 		{
-			0 => header ? "Agenzia Gestione" : _difference?.AgenziaGestione ?? "",
-			1 => header ? "Codice Prodotto" : _difference?.CodiceProdotto ?? "",
-			2 => header ? "Codice Societa" : _difference?.CodiceSocieta ?? "",
-			3 => header ? "Categoria" : _difference?.Categoria ?? "",
-			4 => header ? "Numero Collettiva" : _difference?.NumeroCollettiva ?? "",
-			5 => header ? "Numero Polizza" : _difference?.NumeroPolizza ?? "",
-			6 => header ? "Errore" : _difference?.Errore ?? "",
-			7 => header ? "Tipo Riscatto" : TipoRiscatto(),
-			8 => header ? "Importo Netto\r\nCics" : _difference?.ImportoNettoCics ?? "",
-			9 => header ? "Importo Lordo\r\nCics" : _difference?.ImportoLordoCics ?? "",
-			10 => header ? "Imposte Lordo\r\nCics" : _difference?.ImposteLordoCics ?? "",
-			11 => header ? "Importo Netto\r\nAlbedino" : _difference?.ImportoNettoAlbedino ?? "",
-			12 => header ? "Importo Lordo\r\nAlbedino" : _difference?.ImportoLordoAlbedino ?? "",
-			13 => header ? "Imposte Lordo\r\nAlbedino" : _difference?.ImposteLordoAlbedino ?? "",
-			14 => header ? "Errore Cics" : _difference?.ErroreCics ?? "",
-			15 => header ? "Errore Albedino" : _difference?.ErroreAlbedino ?? "",
+			0 => header ? "Riga" : polizza?.NumeroRiga ?? "",
+			1 => header ? "Input" : polizza?.Input ?? "",
+			2 => header ? "Agenzia Gestione" : _difference?.AgenziaGestione ?? polizza?.AgenziaGestione ?? "",
+			3 => header ? "Codice Prodotto" : _difference?.CodiceProdotto ?? polizza?.CodiceProdotto ?? "",
+			4 => header ? "Codice Societa" : _difference?.CodiceSocieta ?? polizza?.CodiceSocieta ?? "",
+			5 => header ? "Categoria" : _difference?.Categoria ?? polizza?.Categoria ?? "",
+			6 => header ? "Numero Collettiva" : _difference?.NumeroCollettiva ?? polizza?.NumeroCollettiva ?? "",
+			7 => header ? "Numero Polizza" : _difference?.NumeroPolizza ?? polizza?.NumeroPolizza ?? "",
+			8 => header ? "Data Decorrenza" : polizza?.DataDecorrenza ?? "",
+			9 => header ? "Tipi Riscatto" : polizza?.TipiRiscatto ?? "",
+			10 => header ? "Convenzione" : polizza?.Convenzione ?? "",
+			11 => header ? "Errore" : _difference?.Errore ?? "",
+			12 => header ? "Tipo Riscatto Errore" : TipoRiscatto(),
+			13 => header ? "Importo Netto\r\nCics" : _difference?.ImportoNettoCics ?? "",
+			14 => header ? "Importo Lordo\r\nCics" : _difference?.ImportoLordoCics ?? "",
+			15 => header ? "Imposte Lordo\r\nCics" : _difference?.ImposteLordoCics ?? "",
+			16 => header ? "Importo Netto\r\nAlbedino" : _difference?.ImportoNettoAlbedino ?? "",
+			17 => header ? "Importo Lordo\r\nAlbedino" : _difference?.ImportoLordoAlbedino ?? "",
+			18 => header ? "Imposte Lordo\r\nAlbedino" : _difference?.ImposteLordoAlbedino ?? "",
+			19 => header ? "Errore Cics" : _difference?.ErroreCics ?? "",
+			20 => header ? "Errore Albedino" : _difference?.ErroreAlbedino ?? "",
+			21 => header ? "Importo\r\nrParziale" : timesParziale?.ImportoRiscattoParziale ?? "",
+			22 => header ? "Inizio\r\nrTotale" : timesTotale?.Inizio ?? "",
+			23 => header ? "Fine\r\nrTotale" : timesTotale?.Fine ?? "",
+			24 => header ? "Tempo Impiegato\r\nrTotale" : timesTotale?.TempoImpiegato ?? "",
+			25 => header ? "Millisecondi\r\nrTotale" : timesTotale?.Millisecondi ?? "",
+			26 => header ? "Inizio\r\nrParziale" : timesParziale?.Inizio ?? "",
+			27 => header ? "Fine\r\nrParziale" : timesParziale?.Fine ?? "",
+			28 => header ? "Tempo Impiegato\r\nrParziale" : timesParziale?.TempoImpiegato ?? "",
+			29 => header ? "Millisecondi\r\nrParziale" : timesParziale?.Millisecondi ?? "",
 			_ => "",
 		};
 	}
--- AggregaRisultatiTest/Writers/CsvReadDto.cs
+++ AggregaRisultatiTest/Writers/CsvReadDto.cs
@@ -5,35 +5,63 @@ namespace AggregaRisultatiTest.Writers;
 public class CsvReadDto
 {
 	[Index(0)]
-	public string? AgenziaGestione { get; set; }
+	public string? Riga { get; set; }
 	[Index(1)]
-	public string? CodiceProdotto { get; set; }
+	public string? Input { get; set; }
 	[Index(2)]
-	public string? CodiceSocieta { get; set; }
+	public string? AgenziaGestione { get; set; }
 	[Index(3)]
-	public string? Categoria { get; set; }
+	public string? CodiceProdotto { get; set; }
 	[Index(4)]
-	public string? NumeroCollettiva { get; set; }
+	public string? CodiceSocieta { get; set; }
 	[Index(5)]
-	public string? NumeroPolizza { get; set; }
+	public string? Categoria { get; set; }
 	[Index(6)]
-	public string? Errore { get; set; }
+	public string? NumeroCollettiva { get; set; }
 	[Index(7)]
-	public string? TipoRiscatto { get; set; }
+	public string? NumeroPolizza { get; set; }
 	[Index(8)]
-	public string? ImportoNettoCics { get; set; }
+	public string? DataDecorrenza { get; set; }
 	[Index(9)]
-	public string? ImportoLordoCics { get; set; }
+	public string? TipiRiscatto { get; set; }
 	[Index(10)]
-	public string? ImposteLordoCics { get; set; }
+	public string? Convenzione { get; set; }
 	[Index(11)]
-	public string? ImportoNettoAlbedino { get; set; }
+	public string? Errore { get; set; }
 	[Index(12)]
-	public string? ImportoLordoAlbedino { get; set; }
+	public string? TipoRiscatto { get; set; }
 	[Index(13)]
-	public string? ImposteLordoAlbedino { get; set; }
+	public string? ImportoNettoCics { get; set; }
 	[Index(14)]
-	public string? ErroreCics { get; set; }
+	public string? ImportoLordoCics { get; set; }
 	[Index(15)]
+	public string? ImposteLordoCics { get; set; }
+	[Index(16)]
+	public string? ImportoNettoAlbedino { get; set; }
+	[Index(17)]
+	public string? ImportoLordoAlbedino { get; set; }
+	[Index(18)]
+	public string? ImposteLordoAlbedino { get; set; }
+	[Index(19)]
+	public string? ErroreCics { get; set; }
+	[Index(20)]
 	public string? ErroreAlbedino { get; set; }
+	[Index(21)]
+	public string? ImportoRParziale { get; set; }
+	[Index(22)]
+	public string? InizioRTotale { get; set; }
+	[Index(23)]
+	public string? FineRTotale { get; set; }
+	[Index(24)]
+	public string? TempoImpiegatoRTotale { get; set; }
+	[Index(25)]
+	public string? MillisecondiRTotale { get; set; }
+	[Index(26)]
+	public string? InizioRParziale { get; set; }
+	[Index(27)]
+	public string? FineRParziale { get; set; }
+	[Index(28)]
+	public string? TempoImpiegatoRParziale { get; set; }
+	[Index(29)]
+	public string? MillisecondiRParziale { get; set; }
 }
